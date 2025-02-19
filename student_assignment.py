353a216deb4442264de1470515e161ae4034e3cb
Justin1 Chen(陳靖軒) <Justin1_Chen@asus.com>
2025-02-19 17:53:58 +0800

    Double check HW2

--- student_assignment.py
+++ student_assignment.py
@@ -1,4 +1,5 @@
 from langchain_community.document_loaders import PyPDFLoader
+#from langchain_text_splitters import CharacterTextSplitter
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 q1_pdf = "OpenSourceLicenses.pdf"
@@ -18,7 +19,8 @@ def hw02_1(q1_pdf):
     return docs[last_page_index]
     
 def hw02_2(q2_pdf):
-    docs = load_with_PyPdf(q2_pdf)
+    loader = PyPDFLoader(q2_pdf)
+    docs = loader.load()
     text = ""
     for doc in docs:
         text = text + doc.page_content + "\n"
@@ -31,7 +33,40 @@ def hw02_2(q2_pdf):
     result = spliter.create_documents([text])
     #print(len(result))
     return len(result)
+    #print_docs(result)
+    # spliter1 = CharacterTextSplitter(separator="第 \\d+-?\\d* 條\n",
+    #                                 chunk_size=0,
+    #                                 chunk_overlap=0,
+    #                                 is_separator_regex=True,
+    #                                 keep_separator=True)
+    # result1 = spliter1.create_documents([text])
+    # print(len(result1))
+    # print_docs(result1)
+    # spliter2 = CharacterTextSplitter(separator="第 .* 章 .*\n",
+    #                                 chunk_size=0,
+    #                                 chunk_overlap=0,
+    #                                 is_separator_regex=True,
+    #                                 keep_separator=True)
+    # result2 = spliter2.create_documents([text])
+    # print(len(result2))
+    # print_docs(result2)
+
+def print_docs(docs):
+    count = 0
+    for doc in docs:
+        print("----- Doc", count, "begin ------")
+        print("**** Page Content ****")
+        print(len(doc.page_content))
+        print(doc.page_content)
+        print("**********************")
+        print("===== MetaData ====")
+        print(doc.metadata)
+        print("===================")
+        print("----- Doc", count, "end ------")
+        count = count + 1
+        print()
 
 if __name__ == "__main__":
     #hw02_1(q1_pdf)
-    #hw02_2(q2_pdf)
+    hw02_2(q2_pdf)
+    
